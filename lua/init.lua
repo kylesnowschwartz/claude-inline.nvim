6f54a431f911711e86a247de2683038e664c318f
nishu-murmu <nishumurmu017@gmail.com>
2025-07-08 09:14:23 +0530

    feat: adding base prompt modal UI.

---
+++ lua/init.lua
@@ -0,0 +1,29 @@
+local M = {}
+local config = require("ai-companion.config")
+local inputs = require("ai-companion.inputs")
+local layouts = require("ai-companion.layouts")
+
+M.setup = function(user_config)
+  vim.tbl_deep_extend("force", config, user_config)
+end
+
+if config.api_key == "" or config.api_key == nil then
+  inputs.api_key_input:mount()
+end
+
+vim.api.nvim_create_user_command("Companion", function(opts)
+  local args = opts.args
+  if args == "chat" then
+    layouts.gpt_prompt_layout:mount()
+    vim.api.nvim_set_current_win(inputs.user_prompt_popup.winid)
+    vim.api.nvim_win_set_cursor(inputs.user_prompt_popup.winid, { 1, 2 })
+  end
+end, {
+  nargs = 1,
+  desc = "AI Companion commands.",
+  complete = function()
+    return { "chat", "review" }
+  end
+})
+
+return M
